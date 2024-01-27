3cead4fb706e52d1e824df208a06d943eb0323c5
InfiniteBed <lukeeboots@gmail.com>
2024-01-26 19:24:25 -0800

    Fixed memory game

--- index.py
+++ index.py
@@ -38,7 +38,7 @@ async def on_message(message):
     if message.content.startswith('$hello'):
         await msg('Hello!')
 
-    if message.content.lower().startswith('say') and message.author.id == 867261583871836161:
+    if str(message.content).lower().startswith('say') and message.author.id == 867261583871836161:
         tosend = ''
         for x in range(1, (len((message.content).split())-1) + 1):
             tosend += (f' {message.content.split()[x]}')
@@ -46,49 +46,46 @@ async def on_message(message):
     
     if message.content.startswith('Scene 2'):  
         actor = str(message.content).split()[2]
-
+        print(f"--------------------\nStarting Memory Game!")
         for x in range(0, 10):
-            print ('Finding line that matches!')
+            print (f"\nRound {x} of 10!")
             # Get Random Line
-            number = None
-            prevrng = None
-
-            while number == prevrng:
+            number = 1
+            prevrng = 1
+            while number == prevrng and actor.lower() != str(linesdata[number]['Char']).lower():
                 number = random.randint(0, (len(linesdata) - 1) )
-                print(f'Random Number set to {str(number)}')
 
-            if actor.lower() == linesdata[number]['Char'].lower():
+            print('found match')
 
-                # Print Lines
-                messageblock = '```'
-                for x in range(-3, 0):
-                    messageblock += (f"{linesdata[number+x]['Char']}: {linesdata[number+x]['Line']}\n") 
-                messageblock += '```'
-                await msg(messageblock)
+            # Print Lines
+            messageblock = '```'
+            for y in range(-3, 0):
+                messageblock += (f"{linesdata[number+y]['Char'  ]}: {linesdata[number+y]['Line']}\n") 
+            messageblock += '```'
+            await msg(messageblock)
+            print(messageblock)
 
-                await msg(f'*{actor}, what is your line?*')
+            await msg(f'*{actor}, what is your line?*')
+            
+            def check(m):
+                return m.author == message.author
                 
-                def check(m):
-                    return m.author == message.author
-                    
-                
-                try:
-                    guess = await client.wait_for('message', timeout=60.0, check=check)
-                except asyncio.TimeoutError:
-                    await msg(f"Sorry, you took too long. It was `\"{linesdata[number]['Line']}\"`\nWe will cancel your game due to inactivity.")
-                    break
+            
+            try:
+                guess = await client.wait_for('message', timeout=60.0, check=check)
+            except asyncio.TimeoutError:
+                await msg(f"Sorry, you took too long. It was `\"{linesdata[number]['Line']}\"`\nWe will cancel your game due to inactivity.")
+                break
 
-                accuracy = string_similarity(linesdata[number]['Line'], str(guess.content))
+            accuracy = string_similarity(str(linesdata[number]['Line']).lower(), str(guess.content).lower())
 
-                if accuracy >= 80:
-                    await message.channel.send(f'You are right! You got it within {accuracy}% accuracy!')
-                else:
-                    await message.channel.send(f"Oops. It is actually \"{linesdata[number]['Line']}\".")
+            if accuracy >= 80:
+                await message.channel.send(f'You are right! You got it within {accuracy}% accuracy!')
+            else:
+                await message.channel.send(f"Oops. It is actually \"{linesdata[number]['Line']}\".")
 
-                #Reset Random Number
-                number = prevrng
-
-            x+=1
+            #Reset Random Number
+            number = prevrng
 
         
 
